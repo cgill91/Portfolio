ccf3fe3e1bc73abd7f5ba03e7dffb1b70f8ced97
Colton <coltongillmore09@gmail.com>
2022-09-18 14:39:15 -0500

    clean up

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -146,7 +146,7 @@ const Work = () => {
               {/* Hover Effects */}
               <div className='opacity-0 group-hover:opacity-100'>
                 <span className='text-2xl font-bold text-white tracking-wider'>
-                  Name
+                  Portfolio 
                 </span>
                 <div className='pt-8 text-center'>
                   <a href='/'>
